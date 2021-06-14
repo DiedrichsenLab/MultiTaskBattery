90234db6a0ef58b282bbfdb093a9442db4fd6859
ladan <lshahsha@uwo.ca>
2021-06-14 15:09:00 -0400

    added a variable called experiment name to make dirs specific to an experiment

--- experiment_code/constants.py
+++ experiment_code/constants.py
@@ -6,13 +6,16 @@ import os
 response_keys    = ['a', 's', 'd', 'f', 'h', 'j', 'k', 'l']
 response_fingers = ['Pinky', 'Ring', 'Middle', 'Index', 'Index', 'Middle', 'Ring', 'Pinky']
 
-base_dir   = Path(__file__).absolute().parent.parent
-stim_dir   = base_dir / "stimuli"
-target_dir = base_dir / "target_files"
-run_dir    = base_dir / "run_files"
+# change experiment name to a name you've chosen for your own experiment
+experiment_name = 'pontine_7T'
+# change the str inside Path() to a directory of your choise.
+## make sure 'stimuli' and 'experiment_code' folders are placed within your base_dir
+base_dir        = Path('/home/ladan/Documents/Project/mdtb_reduced').absolute()
 
-# This is where the result files are being saved
-raw_dir    = base_dir/ "data"  
+stim_dir   = base_dir / "stimuli"                       # where stimuli for each task are stored
+target_dir = base_dir / experiment_name /"target_files" # contains target files for the task
+run_dir    = base_dir / experiment_name /"run_files"    # contains run files for each session
+raw_dir    = base_dir/ experiment_name / "data"         # This is where the result files are being saved
 
 def dircheck(path2dir):
     """
@@ -27,10 +30,12 @@ def dircheck(path2dir):
     else:
         print(f"{path2dir} already exists")
 
+# use dirtree to make sure you have all the folders needed
 def dirtree():
     """
     Create all the directories if they don't already exist
     """
+    
     fpaths = [raw_dir, stim_dir, target_dir, run_dir]
     for fpath in fpaths:
         dircheck(fpath)
