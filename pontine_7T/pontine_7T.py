e1410e71bc9aefdede27799273aea2b65e4a1341
lshahsha <lshahsha@uwo.ca>
2021-08-28 23:07:05 -0400

    added eye_flag as input option to main

--- pontine_7T/pontine_7T.py
+++ pontine_7T/pontine_7T.py
@@ -45,12 +45,12 @@ def simulate(**kwargs):
 ## change debug to False once you are sure everything is debugged 
 ## make sure that you have changed the screen_res to the res for the subject screen
 ## display mode should also be in extend!
-def main(subj_id, exp_name = "pontine_7T", debug = True, screen_res = [1920, 1080], screen_number = 1):
+def main(subj_id, exp_name = "pontine_7T", debug = True, eye_flag = False):
     # 1. create a class for the experiment
-    Custom_Exp = exp_block.Experiment(exp_name=exp_name, subj_id=subj_id)
+    Custom_Exp = exp_block.Experiment(exp_name=exp_name, subj_id=subj_id, eye_flag=eye_flag)
 
     # 2. get experiment information
-    exp_info = Custom_Exp.set_info(debug = debug, screen_res = screen_res, screen_number = screen_number)
+    exp_info = Custom_Exp.set_info(debug = debug)
 
     # 3. run the run
     Custom_Exp.run()
