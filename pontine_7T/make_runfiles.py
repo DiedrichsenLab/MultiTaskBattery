f6b3b20dbc09a970dc54ff37b5e020cedd3dd6ea
Barafat2 <118376927+Barafat2@users.noreply.github.com>
2023-12-06 18:32:04 -0500

    actionobs targt +task

--- pontine_7T/make_runfiles.py
+++ pontine_7T/make_runfiles.py
@@ -5,7 +5,7 @@ import numpy as np
 
 """ This is an example script to make the run files and trial files for an experiment"""
 
-tasks = ['degraded_passage','theory_of_mind'] # ,'social_prediction','verb_generation'
+tasks = ['degraded_passage','action_observation'] # ,'social_prediction','verb_generation'
 
 #  check if dirs for the tasks and runs exist, if not, make them
 ut.dircheck(const.run_dir)
