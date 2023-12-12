933b3a1658f3429c50d6a65bba52c43972517871
Barafat2 <118376927+Barafat2@users.noreply.github.com>
2023-12-11 21:08:51 -0500

    degraded/intact passag adjustments

--- pontine_7T/make_runfiles.py
+++ pontine_7T/make_runfiles.py
@@ -5,7 +5,7 @@ import numpy as np
 
 """ This is an example script to make the run files and trial files for an experiment"""
 
-tasks = ['nonword_reading','oddball'] # ,'social_prediction','verb_generation'
+tasks = ['intact_passage','degraded_passage'] # ,'social_prediction','verb_generation'
 
 #  check if dirs for the tasks and runs exist, if not, make them
 ut.dircheck(const.run_dir)
