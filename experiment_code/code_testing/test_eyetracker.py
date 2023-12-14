ae5006811bfc4b0bd96463d74662cd03efd2c0d0
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2023-12-14 15:28:29 -0500

    structure

--- experiment_code/code_testing/test_eyetracker.py
+++
@@ -1,6 +0,0 @@
-import pylink
-
-# 1. open a connection to the tracker
-# replace the IP address with None will open a simulated connection
-eye_link = pylink.EyeLink(None)
-# eye_link = pylink.EyeLink('100.1.1.1') # getting the IP address for the eyetracker
