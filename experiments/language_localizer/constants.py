d878d9566aa3be1d78996eea5a7793f0dbd2d1ed
Barafat2 <118376927+Barafat2@users.noreply.github.com>
2023-12-14 21:20:49 -0500

    combined demand grid difficulty

--- experiments/language_localizer/constants.py
+++ experiments/language_localizer/constants.py
@@ -2,6 +2,7 @@
 # it is passed to the Experiment class on initialization
 from pathlib import Path
 import os
+import MultiTaskBattery as mtb
 
 #Necessary definitions for the experiment:
 exp_name = 'pontine_7T'
@@ -15,16 +16,19 @@ exp_dir = Path(os.path.dirname(os.path.realpath(__file__)))   # where the experi
 task_dir = exp_dir / "task_files"  # contains target files for the task
 run_dir    = exp_dir / "run_files"     # contains run files for each session
 data_dir   = exp_dir / "data"          # This is where the result files are being saved
-stim_dir   = exp_dir /'..'/ "stimuli"       # This is where the stimuli are stored
+
+package_dir = Path(os.path.dirname(os.path.dirname(os.path.realpath(mtb.__file__))))
+stim_dir   = package_dir / "stimuli"       # This is where the stimuli are stored
 
 # is the Eye tracker being used
 eye_tracker = False                                     # do you want to do  eyetracking?
 
 # Running in debug mode?
-debug = True                                           # set to True for debugging
+debug = False                                           # set to True for debugging
 
 # Screen settings for subject display
 screen = {}
 screen['size'] = [1100, 800]                             # screen resolution
 screen['fullscr'] = False                               # full screen?
 screen['number'] = 1                                    # 0 = main display, 1 = secondary display
+
