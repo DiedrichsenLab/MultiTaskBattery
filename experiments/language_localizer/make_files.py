5b4da499f5099caf6a4b5188ced734768b6e09c5
Barafat2 <118376927+Barafat2@users.noreply.github.com>
2023-12-17 11:01:54 -0500

    lang loc audnarr not adjacent to passages

--- experiments/language_localizer/make_files.py
+++ experiments/language_localizer/make_files.py
@@ -10,10 +10,15 @@ full_tasks = ['demand_grid','theory_of_mind','verb_generation','degraded_passage
          'action_observation','rest','n_back','romance_movie','sentence_reading','nonword_reading','oddball',\
         'auditory_narrative','tongue_movement','spatial_navigation','finger_sequence']
 
+
 # this is a list of the tasks running while debugging and testing different combos and will be used when the final combo is ready (having both this list and the above
 #is necessary because I have the task_args list defined first then I am putting conditional statements for specific arguments,
 # for the script to run, the conditional stuff needs to run and for the conditional stuff to run all tasks (full_tasks)need to be inside task_args)
-running_tasks = ['auditory_narrative']  # adjust this list as you like to test different combos
+running_tasks = ['demand_grid','theory_of_mind','verb_generation','degraded_passage','intact_passage',\
+         'action_observation','rest','n_back','romance_movie','sentence_reading','nonword_reading','oddball',\
+        'auditory_narrative','tongue_movement','spatial_navigation','finger_sequence']  # adjust this list as you like to test different combos
+
+
 
 #  check if dirs for the tasks and runs exist, if not, make them
 ut.dircheck(const.run_dir)
@@ -21,11 +26,28 @@ for task in running_tasks:
     ut.dircheck(const.task_dir / task)
 
 for r in range(1,9):
-    # making the run files
-    tfiles = [f'{task}_{r:02d}.tsv' for task in running_tasks]
-    T  = mt.make_run_file(running_tasks,tfiles)
-    T.to_csv(const.run_dir / f'run_{r:02d}.tsv',sep='\t',index=False)
-    
+    valid_run_file = False    
+    while not valid_run_file: # this is necessary to make sure that the run files are valid (i.e. no auditory narrative adjacent to intact or degraded passage)
+        # making the run files
+        tfiles = [f'{task}_{r:02d}.tsv' for task in running_tasks]
+        T  = mt.make_run_file(running_tasks,tfiles)
+
+        tasks = T['task_name'].tolist()
+        valid_run_file = True
+
+
+        for i in range(len(tasks) - 1):
+            if tasks[i] == 'auditory_narrative' and tasks[i + 1] in ['intact_passage', 'degraded_passage']:
+                valid_run_file = False
+                break
+            if tasks[i + 1] == 'auditory_narrative' and tasks[i] in ['intact_passage', 'degraded_passage']:
+                valid_run_file = False
+                break
+
+        if valid_run_file:
+            T.to_csv(const.run_dir / f'run_{r:02d}.tsv', sep='\t', index=False)
+            break  # Valid run file found, exit the while loop
+        
     # rewrite task args but with empty dict for all tasks
     task_args = {task: {} for task in full_tasks}
     
