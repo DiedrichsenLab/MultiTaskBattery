7019c0db12167db0ac2397637ea6b409145b0261
Lawdan Shahshahani <44071291+lshahsha@users.noreply.github.com>
2023-12-18 15:53:14 -0500

    fix run_experiment run file find

--- experiments/language_localizer/make_files.py
+++ experiments/language_localizer/make_files.py
@@ -15,7 +15,9 @@ full_tasks = ['demand_grid','theory_of_mind','verb_generation','degraded_passage
 # this is a list of the tasks running while debugging and testing different combos and will be used when the final combo is ready (having both this list and the above
 #is necessary because I have the task_args list defined first then I am putting conditional statements for specific arguments,
 # for the script to run, the conditional stuff needs to run and for the conditional stuff to run all tasks (full_tasks)need to be inside task_args)
-running_tasks = ['theory_of_mind','n_back']  # adjust this list as you like to test different combos
+running_tasks = ['demand_grid','theory_of_mind','verb_generation','degraded_passage','intact_passage',\
+         'action_observation','rest','n_back','romance_movie','sentence_reading','nonword_reading','oddball',\
+        'auditory_narrative','tongue_movement','spatial_navigation','finger_sequence']  # adjust this list as you like to test different combos
 
 # make 30 subject numbers
 subj_list = ['sub-01','sub-02','sub-03','sub-04','sub-05','sub-06','sub-07','sub-08','sub-09','sub-10',\
